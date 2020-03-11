662b73ef9a69edf661a7441cab51f6ba1ee2090b
wesquintana <wesquintana@gmail.com>
2020-03-11 15:06:25 -0600

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+//STORE
 
+//ACTION
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+let store = createStore(counter);
+store.subscribe(() => console.log(store.getState()));
+//DISPATCH
+store.dispatch(increment());
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
