c05fe77b036e8605fd60fc8e14677e6973e2ab73
wesquintana <wesquintana@gmail.com>
2020-03-11 15:46:30 -0600

    adds increment and decrement to counter

--- src/Components/Thing.js
+++ src/Components/Thing.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../Store/Actions";
 function Thing() {
   const counter = useSelector(state => state.counter);
-  return <h1>Counter: {counter}</h1>;
+  const dispatch = useDispatch();
+  return (
+    <div className="Thing">
+      <h1>
+        <button onClick={() => dispatch(increment())}>+</button>Counter:{" "}
+        {counter}
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </h1>
+    </div>
+  );
 }
 export default Thing;
